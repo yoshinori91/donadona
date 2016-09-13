d03afc714823c8d16f5e88f0acb1d485c3b0fe75
Yoshinori Negishi <evr013@d2c.co.jp>
2016-09-13 16:48:51 +0900

    ngif

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,30 +2,33 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-<h2>{{hero.name}} details!!!!</h2>
-<div><label>id: </label>{{hero.id}}</div>
-<div>
-  <label>name: </label>
-  <input value="{{hero.name}}" placeholder="name">
-  <input [(ngModel)]="hero.name" placeholder="name">
+<div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  </div>
 </div>
+
 <h2>My Heroes</h2>
 <ul class="heros">
-  <li *ngFor="let hero of heroes">
-    <span class="badge">{{hero.id}}</span> {{hero.name}}
-  </li>
+<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+  <span class="badge">{{hero.id}}</span> {{hero.name}}
+</li>
 </ul>
   `
 })
+
 export class AppComponent {
   title: string = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-}
+  selectedHero: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
 
 export class Hero {
   id: number;
