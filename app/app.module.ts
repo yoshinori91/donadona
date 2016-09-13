86327dd5f77ad7bd95beae029f28be58f2897e3a
Yoshinori Negishi <evr013@d2c.co.jp>
2016-09-13 19:28:37 +0900

    gotoDetail is not implemented.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,22 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
-import {AppComponent}   from './app.component';
+import {routing} from './app.routing'
+import {AppComponent} from './app.component'
+import {HeroesComponent}   from './heroes.component';
 import {HeroDetailComponent} from "./hero-detail.component";
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   bootstrap: [
